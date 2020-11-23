a9ae8484ff52bce7754174e99d4b0d41dc76a075
Eduardo Dias <carloseduardodiasm@gmail.com>
2020-11-23 18:09:08 -0300

    create user controller

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import userController from './app/controllers/user'
 
 const app = express()
 const PORT = process.env.PORT || 3333
+
+app.use(express.json())
+
+app.post('/users', userController.store)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
