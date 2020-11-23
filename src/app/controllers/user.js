5f3859db73aa312cdaeecc4aeb7660fc632faca8
Eduardo Dias <carloseduardodiasm@gmail.com>
2020-11-23 18:25:50 -0300

    send email in the user controller

--- src/app/controllers/user.js
+++ src/app/controllers/user.js
@@ -1,3 +1,5 @@
+import mail from '../lib/mail'
+
 export default {
 
   async store (req, res) {
@@ -6,6 +8,13 @@ export default {
     const user = {
       name, email, password
     }
+
+    mail.sendMail({
+      from: 'background job test <backgroundjob@test.com.br>',
+      to: `${name} <${email}>`,
+      subject: 'User registration',
+      html: `Hi, ${name}! Welcome to the background jobs of my server :)`
+    })
 
     return res.json(user)
 
