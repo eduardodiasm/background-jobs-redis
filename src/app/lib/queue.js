0683837e8ced43fec072ab0ff7c8bd58aa585290
Eduardo Dias <carloseduardodiasm@gmail.com>
2020-11-23 19:06:45 -0300

    handling error in the sendEmail process

--- src/app/lib/queue.js
+++ src/app/lib/queue.js
@@ -5,4 +5,9 @@ import registrationMail from '../jobs/registrationMail'
 
 const mailQueue = new Queue(registrationMail.key, redisConfig)
 
+mailQueue.on('failed', (job, error) => {
+  console.log('Job failed', job.name, job.data)
+  console.log(error)
+})
+
 export default mailQueue
