eaa68e7040921a9d86bc0d335de44626905e9087
Eduardo Dias <carloseduardodiasm@gmail.com>
2020-11-23 19:25:39 -0300

    add and set up bull-board

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import 'dotenv/config'
 import express from 'express'
+import bullBoard from 'bull-board'
+import queue from './app/lib/queue'
 import userController from './app/controllers/user'
 
 const app = express()
 const PORT = process.env.PORT || 3333
+bullBoard.setQueues(queue.queues.map(queue => queue.bull))
 
 app.use(express.json())
+app.use('/admin/queues', bullBoard.UI)
 
 app.post('/users', userController.store)
 
